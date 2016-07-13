d8756db92efafb3a8fc0f9d32b458dd58a05e849
Kirill Tikhonov <kirill.tikhonov@gmail.com>
2016-07-13 15:08:39 +0300

    Интерпретирует и сливает команды

--- brainfuck/brainfuck-opt.py
+++ brainfuck/brainfuck-opt.py
@@ -10,63 +10,53 @@ class Command(object):
     def __repr__(self):
         return '%s:%d'%(self.char, self.arg)
 
-class Program(object):
-    def __init__(self, code=None):
-        if code:
-            self.load(code)
 
-    def append(self, char):
-        self.code.append(Command(char))
-        if char=='[':
-            self.braces.append(len(self.code)-1)
-        elif char==']':
-            left = self.braces.pop()
-            self.code[left].arg = len(self.code)-1
-            self.code[-1].arg = left
+def load(brainfuck, roll=True):
+    braces = []
+    code = [Command('')]
+    for char in brainfuck:
+        if char in '[]<>.,+-':
+            if roll and char in '<>+-' and char==code[-1].char:
+                code[-1].arg+=1
+            else:
+                code.append(Command(char))
+                if char=='[':
+                    braces.append(len(code)-1)
+                elif char==']':
+                    left = braces.pop()
+                    code[left].arg = len(code)-1
+                    code[-1].arg = left
+    print code
+    return code
 
-    def load(self, code):
-        self.braces = []
-        self.code = [Command('')]
-        for char in code:
-            if char in '[]<>.,+-':
-                self.append(char)
-        # print self.code
 
-    def run(self):
-        tape = [0]*30000
-        xc = 0
-        pc = 0
-        prog_len = len(self.code)
-        while pc<prog_len:
-            cmd = self.code[pc]
-            if cmd.char=='+':
-                tape[xc]+=cmd.arg
-            elif cmd.char=='-':
-                tape[xc]-=cmd.arg
-            elif cmd.char=='>':
-                xc+=cmd.arg
-            elif cmd.char=='<':
-                xc-=cmd.arg
-            elif cmd.char=='.':
-                sys.stdout.write(chr(tape[xc]))
-                # sys.stdout.flush()
-            elif cmd.char==']' and tape[xc]!=0:
-                pc = cmd.arg
-            elif cmd.char=='[' and tape[xc]==0:
-                pc = cmd.arg
-            pc+=1
+def run(code):
+    tape = [0]*30000
+    xc = 0
+    pc = 0
+    prog_len = len(code)
+    while pc<prog_len:
+        cmd = code[pc]
+        if cmd.char=='+':
+            tape[xc]+=cmd.arg
+        elif cmd.char=='-':
+            tape[xc]-=cmd.arg
+        elif cmd.char=='>':
+            xc+=cmd.arg
+        elif cmd.char=='<':
+            xc-=cmd.arg
+        elif cmd.char=='.':
+            sys.stdout.write(chr(tape[xc]))
+            sys.stdout.flush()
+        elif cmd.char==']' and tape[xc]!=0:
+            pc = cmd.arg
+        elif cmd.char=='[' and tape[xc]==0:
+            pc = cmd.arg
+        pc+=1
 
-class Optimizer(Program):
-    def append(self, char):
-        if self.code[-1].char==char and char in '<>+-':
-            self.code[-1].arg+=1
-        else:
-            super(self.__class__, self).append(char)
 
 if __name__=='__main__':
-    from timeit import timeit
+    # from timeit import timeit
 
-    # program = Program(open(sys.argv[1]).read())
-    program = Optimizer(open(sys.argv[1]).read())
-    # print timeit(program.run, number=1)
-    program.run()
+    opt = True # False
+    run(load(open(sys.argv[1]).read(), opt))
