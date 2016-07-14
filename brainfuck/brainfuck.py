d59ced86193a82b80228eb3ead842b165e30251b
Kirill Tikhonov <kirill.tikhonov@gmail.com>
2016-07-14 15:03:30 +0300

    Компилятор в Python

--- brainfuck/brainfuck.py
+++ brainfuck/brainfuck.py
@@ -20,7 +20,7 @@ class Brainfuck(object):
 
     def load(self, brainfuck, optimize=True):
         braces = []
-        self.code = code = [Command('')]
+        self.code = code = [Command('begin')]
         for char in brainfuck:
             if char in '[]<>.,+-':
                 if optimize and char in '<>+-' and char==code[-1].char:
@@ -33,6 +33,7 @@ class Brainfuck(object):
                         left = braces.pop()
                         code[left].arg = len(code)-1
                         code[-1].arg = left
+        self.code.append(Command('end'))
 
     def run(self):
         tape = [0]*30000
@@ -60,21 +61,112 @@ class Brainfuck(object):
             pc+=1
 
 
+class Compiler(Brainfuck):
+    def __init__(self, brainfuck=None, optimize=True):
+        super(Compiler, self).__init__(brainfuck, optimize)
+        self.commands = {}
+        self.define_lang()
+
+    # Определение команды на другом языке
+    class Definition(object):
+        # src - исходник команды Brainfuck на другом языке.
+        # src_opt - исходник команды с аргументом. требует %d
+        # indent - сдвиг.
+        def __init__(self, src, src_arg, indent):
+            src_has_arg = src.count('%d') if src else 0
+            self.src = src%1 if src_has_arg else src
+            self.src_arg = src_arg if src_arg else src
+            self.indent = indent
+
+    def define(self, char, src, src_arg=None, indent=0):
+        self.commands[char] = Compiler.Definition(src, src_arg, indent)
+
+    # Определение соответствий команд Brainfuck конкретному языку
+    def define_lang(self):
+        pass
+
+    def write(self, f, src, indent):
+        if src:
+            f.write((u'\n'+src).replace(u'\n', u'\n'+u'    '*indent))
+
+    def compile(self, f):
+        indent = 0
+        for cmd in self.code:
+            try:
+                d = self.commands[cmd.char]
+                if d.indent:
+                    self.write(f, d.src, indent)
+                    indent+=d.indent
+                elif cmd.arg>1:
+                    self.write(f, d.src_arg%cmd.arg, indent)
+                else:
+                    self.write(f, d.src, indent)
+            except KeyError:
+                pass
+
+
+class PythonCompiler(Compiler):
+    def define_lang(self):
+        self.define('begin',
+u'''# coding=utf-8
+# Автоматически скомпилировано из Brainfuck.
+from sys import stdout
+tape = [0]*32000
+xc = 0
+''')
+        self.define('+', u'tape[xc]+=%d')
+        self.define('-', u'tape[xc]-=%d')
+        self.define('.', u'stdout.write(chr(tape[xc])); stdout.flush()')
+        self.define('>', u'xc+=%d')
+        self.define('<', u'xc-=%d')
+        self.define('[', u'while tape[xc]:', indent=1)
+        self.define(']', None, indent=-1)
+
+
 if __name__=='__main__':
     import codecs
+    import subprocess
 
+    source_filename = None
     optimize = False
-    filename = None
+    compiler = None
+    ext = ''            # расширение нового файла
+    run = True          # исполнять
+    run_command = None  # команда для запуска скомпилированого файла
     for arg in sys.argv[1:]:
-        if arg=='-o' or arg=='-O':
+        if arg=='-o':
             optimize = True
-        else:
-            filename = arg
+        elif arg=='.':
+            run = False
+        elif arg=='python':
+            compiler = PythonCompiler
+            ext = '.py'
+            run_command = u'python %s'
+        elif arg.endswith('.b'):
+            source_filename = arg
 
-    if filename:
-        with codecs.open(filename, 'r', 'utf-8') as f:
-            brainfuck = Brainfuck(f.read(), optimize)
-        brainfuck.run()
+    if source_filename:
+        with codecs.open(source_filename, 'r', 'utf-8') as f:
+            source = f.read()
+
+        if compiler:
+            output_filename = source_filename.replace('.b', ext)
+            with codecs.open(output_filename, 'w', 'utf-8') as f:
+                compiler(source, optimize).compile(f)
+                if run_command and run:
+                    subprocess.call(run_command%output_filename, shell=True)
+                    subprocess.call(u'rm %s'%output_filename, shell=True)
+                else:
+                    print source_filename, '->', output_filename
+        else:
+            Brainfuck(source, optimize).run()
     else:
-        print '.\\brainfuck.py source.b'.ljust(40), 'исполнить source.b'
-        print '.\\brainfuck.py -o source.b'.ljust(40), '-o включает оптимизацию'
+        for example, comment in (
+            ('%s source.b', 'исполнить source.b интерпретатором'),
+            ('%s -o source.b', '-o включает оптимизацию'),
+            ('%s python source.b', 'перевести на Python и запустить'),
+            ('%s emojicode source.b', 'перевести на Emojicode и запустить'),
+            ('%s c source.b', 'перевести на C и запустить'),
+            ('%s python source.b .', 'сохранить исходник и не запускать'),
+        ):
+            print example.ljust(25)%__file__, comment
--- brainfuck/squares.py
+++ brainfuck/squares.py
@@ -5,174 +5,105 @@ import sys
 tape = [0]*32000
 xc = 0
 
-tape[xc]+=1
-tape[xc]+=1
-tape[xc]+=1
-tape[xc]+=1
+tape[xc]+=4
 while tape[xc]:
-	xc+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	xc-=1
-	tape[xc]-=1
+    xc+=1
+    tape[xc]+=5
+    xc-=1
+    tape[xc]-=1
 xc+=1
 while tape[xc]:
-	xc-=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	tape[xc]+=1
-	xc+=1
-	tape[xc]-=1
+    xc-=1
+    tape[xc]+=5
+    xc+=1
+    tape[xc]-=1
 tape[xc]+=1
 xc-=1
 tape[xc]+=1
 while tape[xc]:
-	xc+=1
-	while tape[xc]:
-		xc+=1
-		tape[xc]+=1
-		xc+=1
-		tape[xc]+=1
-		xc-=1
-		xc-=1
-		tape[xc]-=1
-	tape[xc]+=1
-	tape[xc]+=1
-	xc+=1
-	xc+=1
-	while tape[xc]:
-		xc-=1
-		xc-=1
-		tape[xc]+=1
-		xc+=1
-		xc+=1
-		tape[xc]-=1
-	xc+=1
-	xc+=1
-	xc+=1
-	while tape[xc]:
-		tape[xc]-=1
-	tape[xc]+=1
-	tape[xc]+=1
-	xc+=1
-	while tape[xc]:
-		tape[xc]-=1
-	tape[xc]+=1
-	xc+=1
-	xc+=1
-	xc+=1
-	tape[xc]+=1
-	while tape[xc]:
-		while tape[xc]:
-			tape[xc]-=1
-		tape[xc]+=1
-		tape[xc]+=1
-		tape[xc]+=1
-		tape[xc]+=1
-		tape[xc]+=1
-		tape[xc]+=1
-		xc+=1
-		xc+=1
-		xc+=1
-	xc-=1
-	xc-=1
-	xc-=1
-	while tape[xc]:
-		while tape[xc]:
-			xc-=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			xc-=1
-			tape[xc]+=1
-			tape[xc]+=1
-			xc+=1
-			xc+=1
-			tape[xc]-=1
-		tape[xc]+=1
-		xc-=1
-		sys.stdout.write(chr(tape[xc]))
-		xc-=1
-		while tape[xc]:
-			xc+=1
-			tape[xc]-=1
-			tape[xc]-=1
-			tape[xc]-=1
-			tape[xc]-=1
-			xc-=1
-			tape[xc]-=1
-		xc-=1
-	xc-=1
-	xc-=1
-	while tape[xc]:
-		xc+=1
-		xc+=1
-		xc+=1
-		xc+=1
-		xc+=1
-		while tape[xc]:
-			xc+=1
-			xc+=1
-			xc+=1
-			while tape[xc]:
-				tape[xc]-=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			xc-=1
-			while tape[xc]:
-				xc+=1
-				tape[xc]-=1
-				xc-=1
-				tape[xc]-=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			tape[xc]+=1
-			xc+=1
-			while tape[xc]:
-				tape[xc]-=1
-				while tape[xc]:
-					xc-=1
-					tape[xc]-=1
-					xc+=1
-					tape[xc]-=1
-				tape[xc]+=1
-				while tape[xc]:
-					xc-=1
-					xc-=1
-					xc-=1
-			xc-=1
-			while tape[xc]:
-				xc+=1
-				tape[xc]+=1
-				xc-=1
-				tape[xc]-=1
-			xc+=1
-		xc-=1
-		xc-=1
-		tape[xc]-=1
-	xc-=1
-	xc-=1
-	tape[xc]-=1
+    xc+=1
+    while tape[xc]:
+        xc+=1
+        tape[xc]+=1
+        xc+=1
+        tape[xc]+=1
+        xc-=2
+        tape[xc]-=1
+    tape[xc]+=2
+    xc+=2
+    while tape[xc]:
+        xc-=2
+        tape[xc]+=1
+        xc+=2
+        tape[xc]-=1
+    xc+=3
+    while tape[xc]:
+        tape[xc]-=1
+    tape[xc]+=2
+    xc+=1
+    while tape[xc]:
+        tape[xc]-=1
+    tape[xc]+=1
+    xc+=3
+    tape[xc]+=1
+    while tape[xc]:
+        while tape[xc]:
+            tape[xc]-=1
+        tape[xc]+=6
+        xc+=3
+    xc-=3
+    while tape[xc]:
+        while tape[xc]:
+            xc-=1
+            tape[xc]+=8
+            xc-=1
+            tape[xc]+=2
+            xc+=2
+            tape[xc]-=1
+        tape[xc]+=1
+        xc-=1
+        sys.stdout.write(chr(tape[xc]))
+        xc-=1
+        while tape[xc]:
+            xc+=1
+            tape[xc]-=4
+            xc-=1
+            tape[xc]-=1
+        xc-=1
+    xc-=2
+    while tape[xc]:
+        xc+=5
+        while tape[xc]:
+            xc+=3
+            while tape[xc]:
+                tape[xc]-=1
+            tape[xc]+=9
+            xc-=1
+            while tape[xc]:
+                xc+=1
+                tape[xc]-=1
+                xc-=1
+                tape[xc]-=1
+            tape[xc]+=9
+            xc+=1
+            while tape[xc]:
+                tape[xc]-=1
+                while tape[xc]:
+                    xc-=1
+                    tape[xc]-=1
+                    xc+=1
+                    tape[xc]-=1
+                tape[xc]+=1
+                while tape[xc]:
+                    xc-=3
+            xc-=1
+            while tape[xc]:
+                xc+=1
+                tape[xc]+=1
+                xc-=1
+                tape[xc]-=1
+            xc+=1
+        xc-=2
+        tape[xc]-=1
+    xc-=2
+    tape[xc]-=1
