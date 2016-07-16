2b60ee8e8f06c2c97a716e26300e0a233fc2d128
Kirill Tikhonov <kirill.tikhonov@gmail.com>
2016-07-16 11:54:26 +0300

    Укороченный тест ошибки в словаре

--- bugs/dict.py
+++ bugs/dict.py
@@ -1,18 +1,18 @@
 import subprocess
 
 def pretty(dict):
-    return ' '.join(['%d:%d'%(key, value) for key, value in dict.iteritems()])
+    return ', '.join(['%d:%d'%(key, value) for key, value in dict.iteritems()])
 
 def diff(a, b):
     return dict(set(b.items()) - set(a.items()) | set(a.items()) - set(b.items()))
 
-d = old = {}
+d = {}
 for line in subprocess.Popen('emojicode dict.emojib', shell=True, stdout=subprocess.PIPE).stdout.readlines():
     line = line.strip()
     if line:
-        numbers = [int(x) for x in reversed(line.split(' '))]
+        old_d = d.copy()
 
-        old = d.copy()
+        numbers = [int(x) for x in reversed(line.split(' '))]
 
         key, value = numbers.pop(), numbers.pop()
         print 'dict[%d] = %d'%(key, value)
@@ -21,6 +21,6 @@ for line in subprocess.Popen('emojicode dict.emojib', shell=True, stdout=subproc
             key, value = numbers.pop(), numbers.pop()
             d[key] = value
 
-        print 'dict =', pretty(d)
-        print 'diff =', pretty(diff(d, old))
+        print 'dict:', pretty(d)
+        print 'diff:', pretty(diff(d, old_d))
         print
